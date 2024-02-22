1b44f3e5fcb3601e542bd2ac9043a2568ef47cf1
Adriano <adrianovendramini@hotmail.com>
2024-02-22 08:51:57 -0300

    Crud postagem

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,5 +1,8 @@
 import homeLogo from '../../assets/home.jpg'
 
+import ListaPostagens from '../../components/postagens/listaPostagens/ListaPostagens'
+import ModalPostagem from '../../components/postagens/modalPostagem/ModalPostagem'
+
 function Home() {
   return (
     <>
@@ -7,9 +10,10 @@ function Home() {
         <div className="container grid grid-cols-2 text-white">
           <div className="flex flex-col gap-4 items-center justify-center py-4">
             <h2 className="text-5xl font-bold">Seja bem vinde!</h2>
-            <p className="text-xl">Fala, poste e sonhe aqui!</p>
+            <p className="text-xl">Expresse aqui seus pensamentos e opniões</p>
 
             <div className="flex justify-around gap-4">
+              <ModalPostagem />
               <button className="rounded bg-white text-blue-800 py-2 px-4">
                 Ver postagens
               </button>
@@ -21,6 +25,7 @@ function Home() {
           </div>
         </div>
       </div>
+      <ListaPostagens />
     </>
   )
 }
