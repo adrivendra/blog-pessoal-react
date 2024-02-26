13c08d0c38f5c2030e01f79b6b9b30e36caffbb9
Adriano <adrianovendramini@hotmail.com>
2024-02-26 11:11:10 -0300

    CRUD Postagem

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,7 +1,7 @@
 import homeLogo from '../../assets/home.jpg'
-
 import ListaPostagens from '../../components/postagens/listaPostagens/ListaPostagens'
 import ModalPostagem from '../../components/postagens/modalPostagem/ModalPostagem'
+import { Link } from 'react-router-dom'
 
 function Home() {
   return (
@@ -14,9 +14,11 @@ function Home() {
 
             <div className="flex justify-around gap-4">
               <ModalPostagem />
-              <button className="rounded bg-white text-blue-800 py-2 px-4">
-                Ver postagens
-              </button>
+              <Link to="/postagens" className="hover:underline">
+                <button className="rounded bg-white text-blue-800 py-2 px-4">
+                  Ver postagens
+                </button>
+              </Link>
             </div>
           </div>
 
