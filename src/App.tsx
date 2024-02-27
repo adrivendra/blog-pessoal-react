6d031491a52d42e579a105c89aa2c5dba2e41af2
Adriano <adrianovendramini@hotmail.com>
2024-02-27 08:46:12 -0300

    Perfil e Alertas!

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,30 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
 import './App.css'
-
-import Navbar from './components/navbar/Navbar'
-import Footer from './components/footer/Footer'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import 'react-toastify/dist/ReactToastify.css'
 import Login from './pages/login/Login'
-import Cadastro from './pages/cadastro/Cadastro'
-import Home from './pages/home/Home'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { AuthProvider } from './contexts/AuthContext'
+import { ToastContainer } from 'react-toastify'
+import Cadastro from './pages/cadastro/Cadastro'
+import Navbar from './components/navbar/Navbar'
+import Home from './pages/home/Home'
+import Footer from './components/footer/Footer'
 import ListaTemas from './components/temas/listaTemas/ListaTemas'
 import FormularioTema from './components/temas/formularioTema/FormularioTema'
 import DeletarTema from './components/temas/deletarTema/DeletarTema'
 import ListaPostagens from './components/postagens/listaPostagens/ListaPostagens'
 import FormularioPostagem from './components/postagens/formularioPostagem/FormularioPostagem'
+
+import Perfil from './pages/perfil/Perfil'
 import DeletarPostagem from './components/postagens/deletarPostagem/deletarPostagem'
 
 function App() {
   return (
     <>
       <AuthProvider>
+        <ToastContainer />
         <BrowserRouter>
           <Navbar />
           <div className="min-h-[80vh]">
@@ -43,6 +50,7 @@ function App() {
                 path="/deletarPostagem/:id"
                 element={<DeletarPostagem />}
               />
+              <Route path="/perfil" element={<Perfil />} />
             </Routes>
           </div>
           <Footer />
@@ -51,4 +59,5 @@ function App() {
     </>
   )
 }
+
 export default App
